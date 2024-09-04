d8aa38c30160d38dfe8e1c7eba722d2d46aed5d4
shahul-github-s <shahul.github@gmail.com>
2024-09-04 09:30:00 +0530

    comit

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,105 +1,105 @@
 const ROUTES = {
-    main: [
+  main: [
+    {
+      name: "Dashboard",
+      icon: "objects-column",
+      notifications: 2,
+      links: [
         {
-            name: 'Dashboard',
-            icon: 'objects-column',
-            notifications: 2,
-            links: [
-                {
-                    path: '/',
-                    name: 'Dashboard A'
-                },
-                {
-                    path: '/dashboard-b',
-                    name: 'Dashboard B'
-                },
-                {
-                    path: '/dashboard-c',
-                    name: 'Dashboard C'
-                },
-                {
-                    path: '/dashboard-d',
-                    name: 'Dashboard D'
-                }
-            ]
+          path: "/",
+          name: "Dashboard A",
         },
         {
-            name: 'Products',
-            icon: 'bag-shopping',
-            links: [
-                {
-                    path: '/products',
-                    name: 'Product List'
-                },
-                {
-                    path: '/product',
-                    name: 'Product Page'
-                },
-                {
-                    path: '/create-product',
-                    name: 'Create Product'
-                }
-            ]
+          path: "/dashboard-b",
+          name: "Dashboard B",
         },
         {
-            name: 'Orders',
-            icon: 'square-list',
-            links: [
-                {
-                    path: '/orders',
-                    name: 'Orders Table'
-                },
-                {
-                    path: '/order-details',
-                    name: 'Order Details'
-                },
-                {
-                    path: '/invoice',
-                    name: 'Invoice'
-                }
-            ]
+          path: "/dashboard-c",
+          name: "Dashboard C",
         },
         {
-            path: '/sales',
-            name: 'Sales',
-            icon: 'badge-percent'
+          path: "/dashboard-d",
+          name: "Dashboard D",
+        },
+      ],
+    },
+    {
+      name: "Products",
+      icon: "bag-shopping",
+      links: [
+        {
+          path: "/products",
+          name: "Product List",
         },
         {
-            path: '/reviews',
-            name: 'Reviews',
-            icon: 'message-quote'
+          path: "/product",
+          name: "Product Page",
         },
         {
-            name: 'Pages',
-            icon: 'layer-group',
-            links: [
-                {
-                    path: '/sign-in',
-                    name: 'Sign In'
-                },
-                {
-                    path: '/sign-up',
-                    name: 'Sign Up'
-                },
-                {
-                    path: '/404',
-                    name: 'Page 404'
-                }
-            ]
-        }
-    ],
-    secondary: [
+          path: "/create-product",
+          name: "Create Product",
+        },
+      ],
+    },
+    {
+      name: "Orders",
+      icon: "square-list",
+      links: [
         {
-            path: '/settings',
-            name: 'Settings',
-            icon: 'gear'
+          path: "/orders",
+          name: "Orders Table",
         },
         {
-            path: '/sign-in',
-            name: 'Logout',
-            icon: 'arrow-right-from-bracket'
-        }
-    ]
-}
+          path: "/order-details",
+          name: "Order Details",
+        },
+        {
+          path: "/invoice",
+          name: "Invoice",
+        },
+      ],
+    },
+    {
+      path: "/sales",
+      name: "Sales",
+      icon: "badge-percent",
+    },
+    {
+      path: "/reviews",
+      name: "Reviews",
+      icon: "message-quote",
+    },
+    {
+      name: "Pages",
+      icon: "layer-group",
+      links: [
+        {
+          path: "/sign-in",
+          name: "Sign In",
+        },
+        {
+          path: "/sign-up",
+          name: "Sign Up",
+        },
+        {
+          path: "/404",
+          name: "Page 404",
+        },
+      ],
+    },
+  ],
+  secondary: [
+    {
+      path: "/settings",
+      name: "Settings",
+      icon: "gear",
+    },
+    {
+      path: "/sign-in",
+      name: "Logout",
+      icon: "arrow-right-from-bracket",
+    },
+  ],
+};
 
-export default ROUTES
+export default ROUTES;
--- src/pages/DashboardA.jsx
+++ src/pages/DashboardA.jsx
@@ -1,26 +1,30 @@
-import AppBar from '@components/AppBar';
-import AppGrid from '@components/AppGrid';
-import Statistics from '@widgets/Statistics';
-import SalesAnalyticsArea from '@widgets/SalesAnalyticsArea';
-import SalesByCategory from '@widgets/SalesByCategory';
-import TopSelling from '@widgets/TopSelling';
-import TrendingProduct from '@widgets/TrendingProduct';
+import AppBar from "@components/AppBar";
+import AppGrid from "@components/AppGrid";
+import Statistics from "@widgets/Statistics";
+import SalesAnalyticsArea from "@widgets/SalesAnalyticsArea";
+import SalesByCategory from "@widgets/SalesByCategory";
+import TopSelling from "@widgets/TopSelling";
+import TrendingProduct from "@widgets/TrendingProduct";
 
 const widgets = {
-    statistics: <Statistics/>,
-    sales_analytics: <SalesAnalyticsArea/>,
-    sales_by_category: <SalesByCategory/>,
-    top_selling: <TopSelling/>,
-    trending_product: <TrendingProduct/>,
-}
+  statistics: <Statistics />,
+  sales_analytics: <SalesAnalyticsArea />,
+  sales_by_category: <SalesByCategory />,
+  top_selling: <TopSelling />,
+  trending_product: <TrendingProduct />,
+};
 
 const DashboardA = () => {
-    return (
-        <>
-            <AppBar title="Dashboard"/>
-            <AppGrid id="dashboard_a" widgets={widgets} cols={{xl: 4, lg: 3, md: 2}}/>
-        </>
-    )
-}
+  return (
+    <>
+      <AppBar title="AIMS Sales Dashboard" />
+      <AppGrid
+        id="dashboard_a"
+        widgets={widgets}
+        cols={{ xl: 4, lg: 3, md: 2 }}
+      />
+    </>
+  );
+};
 
-export default DashboardA
+export default DashboardA;
